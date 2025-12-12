165a3821bd1a3d9619d79df2929ddc41cfc66759
Régys Borges da Silveira <FO849151@inter.co>
2025-12-12 14:36:52 -0300

    .

--- Services/Service.DTO.Base.pas
+++ Services/Service.DTO.Base.pas
@@ -21,9 +21,8 @@ var
   LPropInfo: PPropInfo;
   LJsonVal: ISuperObject;
 begin
-  if AJson = nil then Exit;
-
-  if AJson = nil then Exit;
+  if AJson = nil then
+    Exit;
 
   if ObjectFindFirst(AJson, LIter) then
   try
@@ -36,7 +35,10 @@ begin
 
         case LPropInfo^.PropType^.Kind of
           tkInteger, tkInt64:
-            SetOrdProp(Self, LPropInfo, LJsonVal.AsInteger);
+            begin
+              SetOrdProp(Self, LPropInfo, LJsonVal.AsInteger);
+            end;
+
           tkFloat:
             begin
               if LJsonVal.DataType = stString then
@@ -44,11 +46,17 @@ begin
               else
                 SetFloatProp(Self, LPropInfo, LJsonVal.AsDouble);
             end;
+
           tkString, tkLString, tkWString:
-            SetStrProp(Self, LPropInfo, LJsonVal.AsString);
+            begin
+              SetStrProp(Self, LPropInfo, LJsonVal.AsString);
+            end;
+
           tkEnumeration:
-             if GetTypeData(LPropInfo^.PropType^)^.BaseType^ = TypeInfo(Boolean) then
-               SetOrdProp(Self, LPropInfo, Integer(LJsonVal.AsBoolean));
+            begin
+              if GetTypeData(LPropInfo^.PropType^)^.BaseType^ = TypeInfo(Boolean) then
+                SetOrdProp(Self, LPropInfo, Integer(LJsonVal.AsBoolean));
+            end;
         end;
       end;
     until not ObjectFindNext(LIter);
